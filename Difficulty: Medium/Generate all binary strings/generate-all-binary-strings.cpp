03dafb006c3ded9ec9ea505df8b05d4c9f9316d0
Jaycodebyte <singhjaykishor7505@gmail.com>
2025-09-30 19:59:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Generate all binary strings/generate-all-binary-strings.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+   //Jay
+     vector<string> binstr(int n) {
+        vector<string> result;
+        int total = 1 << n;  // 2^n
+
+        for (int i = 0; i < total; i++) {
+            string s = bitset<20>(i).to_string();
+            result.push_back(s.substr(20 - n));   // take last n bits
+        }
+        return result;
+    }
+};
