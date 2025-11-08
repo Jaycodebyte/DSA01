8809fb41a9f48c819216e3d4208d1e1d86c70acc
Jaycodebyte <singhjaykishor7505@gmail.com>
2025-11-08 20:00:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Number of paths in a matrix with k coins/number-of-paths-in-a-matrix-with-k-coins.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+  //Jay
+    int n, m;
+    int dp[101][101][101];
+
+    int solve(vector<vector<int>>& mat, int i, int j, int k) {
+        if (i >= n || j >= m || k < 0) return 0;
+        if (i == n - 1 && j == m - 1)
+            return (k == mat[i][j]) ? 1 : 0;
+
+        if (dp[i][j][k] != -1) return dp[i][j][k];
+
+        int remaining = k - mat[i][j];
+        int down = solve(mat, i + 1, j, remaining);
+        int right = solve(mat, i, j + 1, remaining);
+
+        return dp[i][j][k] = (down + right);
+    }
+
+    int numberOfPath(vector<vector<int>>& mat, int k) {
+        n = mat.size();
+        m = mat[0].size();
+        memset(dp, -1, sizeof(dp));
+        return solve(mat, 0, 0, k);
+    }
+};
