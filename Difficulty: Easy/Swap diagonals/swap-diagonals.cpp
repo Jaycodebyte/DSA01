611e882b6ba7f583928d9f3797dd56d26ca3539f
Jaycodebyte <singhjaykishor7505@gmail.com>
2025-12-13 21:04:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Swap diagonals/swap-diagonals.cpp
@@ -0,0 +1,10 @@
+class Solution {
+  public:
+  //Jay
+   void swapDiagonal(vector<vector<int>> &mat) {
+        int n = mat.size();
+        for(int i = 0; i < n; i++) {
+            swap(mat[i][i], mat[i][n - 1 - i]);
+        }
+    }
+};
