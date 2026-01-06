595fdeda5844512673ffdc606f164d2175352d8f
Jaycodebyte <singhjaykishor7505@gmail.com>
2026-01-06 21:16:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Max Xor Subarray of size K/max-xor-subarray-of-size-k.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+  //Jay
+   int maxSubarrayXOR(vector<int>& arr, int k) {
+ 
+        int n = arr.size(), ans = 0;
+        for(int i = 0, x = 0; i < n; i++) {
+            x ^= arr[i];
+            if(i - k >= 0) x ^= arr[i - k];
+            if(i >= k - 1) ans = max(ans, x);
+        }
+        return ans;
+    }
+};
