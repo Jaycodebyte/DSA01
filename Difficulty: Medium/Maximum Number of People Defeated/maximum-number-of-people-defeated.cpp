0d76e146647c8e52c741a5ca7c766c84b55d64cc
Jaycodebyte <singhjaykishor7505@gmail.com>
2026-06-23 21:09:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Maximum Number of People Defeated/maximum-number-of-people-defeated.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+  //Jay
+    int maxPeopleDefeated(int p) {
+        // code here
+        int c = 0;
+        for(int i=1;p>=i*i;i++){
+            p = p-(i*i);
+            c++;
+        }
+        return c;
+    }
+};
